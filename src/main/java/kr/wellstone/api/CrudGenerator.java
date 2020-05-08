fe7820e15910b1456277f845024835322d28cc68
hsoh-ymtech <hsoh@ymtech.co.kr>
2020-05-08 18:17:14 +0900

    javapoet 학습

--- src/main/java/kr/wellstone/api/CrudGenerator.java
+++ src/main/java/kr/wellstone/api/CrudGenerator.java
@@ -1,30 +1,94 @@
 package kr.wellstone.api;
 
 import com.squareup.javapoet.*;
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
 
 import javax.lang.model.element.Modifier;
 import java.io.File;
 import java.io.IOException;
 
 public class CrudGenerator {
+
     public static void main(String[] args) throws IOException {
-        MethodSpec main = MethodSpec.methodBuilder("main")
-                .addModifiers(Modifier.PUBLIC, Modifier.STATIC)
-                .returns(void.class)
-                .addParameter(String[].class, "args")
-                .addStatement("$T.out.println($S)", System.class, "Hello, JavaPoet!")
+        String packageName = "kr.wellstone.api";
+        defaultSetup(packageName);
+    }
+
+    /**
+     * @param packageName package name
+     * @implNote Generate default setup class
+     * config - ApplicationConfig
+     * common - ResponseData, ResponseDataType, MessageSourceImpl, GlobalExceptionController
+     * TODO...
+     */
+    public static void defaultSetup(String packageName) throws IOException {
+        String configPackageName = packageName + ".config";
+        MethodSpec modelMapper = MethodSpec.methodBuilder("modelMapper")
+                .returns(ModelMapper.class)
                 .build();
 
-        TypeSpec helloWorld = TypeSpec.classBuilder("HelloWorld")
-                .addModifiers(Modifier.PUBLIC, Modifier.FINAL)
-                .addMethod(main)
+        TypeSpec applicationConfig = TypeSpec.classBuilder("ApplicationConfig")
+                .addModifiers(Modifier.PUBLIC)
+                .addMethod(modelMapper)
                 .build();
 
-        JavaFile javaFile = JavaFile.builder("kr.wellstone.api", helloWorld)
-                .build();
-
-//        File file = new File("src/main/java/");
-//        javaFile.writeTo(file);
+        JavaFile javaFile = JavaFile.builder(packageName, applicationConfig).build();
         javaFile.writeTo(System.out);
+//        File file = new File(configPackageName);
+//        javaFile.writeTo(file);
     }
 }
+
+
+/*
+@Slf4j
+@Configuration
+@RequiredArgsConstructor
+public class ApplicationConfig {
+
+    private final Environment env;
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("messages/message");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+
+    @Bean
+    public ApplicationRunner applicationRunner() {
+        return new ApplicationRunner() {
+            @Autowired
+            LeshanServer leshanServer;
+
+            @Override
+            public void run(ApplicationArguments args) {
+                if (!Objects.equals(env.getProperty("test.code"), "true")) {
+                    leshanServer.start();
+//                    gatewayRepository.findAll().forEach(gateway -> {
+//                        try {
+//                            ((EditableSecurityStore) leshanServer.getSecurityStore())
+//                                    .add(SecurityInfo.newPreSharedKeyInfo(gateway.getEndpoint(), gateway.getEndpoint(), gateway.getPsk().getBytes()));
+//                        } catch (NonUniqueSecurityInfoException e) {
+//                            e.printStackTrace();
+//                        }
+//                    });
+                }
+            }
+        };
+    }
+}
+
+ */
